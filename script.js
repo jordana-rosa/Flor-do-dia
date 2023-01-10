e4fa57fdd29a184a3726cd48abf24334d0395b13
jordana rosa <jordananabarbosa@hotmail.com>
2023-01-10 20:55:10 -0300

    Iniciando um projeto da materia de construções de página

---
+++ script.js
@@ -0,0 +1,10 @@
+function escreveTexto() {
+    let x = document.querySelector("#texto");
+    x.innerText = 'Junte-se ao grupo colorido!';
+
+}
+
+function voltaTexto() {
+    let x = document.querySelector('#texto');
+    x.innerText = 'Agis, Andrezza e Jordana';
+}
